3e3d17d7d03d3c15ac52735249723bc10a6b294d
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-03-25 22:54:35 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -44,8 +44,7 @@ case "$choice" in
     mkdir ./bp-tmp
     cd ./bp-tmp
     wget -q "$_URL" -O "install.sh"
-    source install.sh
-        install_bp
+bash ./bp-tmp/install.sh
         rm -f ./install.sh
         ;;
     2)
